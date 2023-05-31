0d95ad5014003bb92552b3fb7a240fadc68f3d03
dharmikdabgar381 <dharmikdabgar381@gmail.com>
2023-05-31 16:07:20 +0530

    product-import 0.3.0

--- app/code/local/Dharmik/Idx/Block/Adminhtml/Idx/Edit/Tab/Form.php
+++ app/code/local/Dharmik/Idx/Block/Adminhtml/Idx/Edit/Tab/Form.php
@@ -1,75 +1,15 @@
 <?php
-class Ccc_Vendor_Block_Adminhtml_Vendor_Edit_Tab_Form extends Mage_Adminhtml_Block_Widget_Form
+class Dharmik_Idx_Block_Adminhtml_Idx_Edit_Tab_Form extends Mage_Adminhtml_Block_Widget_Form
 {
-	protected function _prepareForm()
-	{
-		$form = new Varien_Data_Form();
-		$this->setForm($form);
-		$vendorField = $form->addFieldset('vendor_form',array('legend'=>Mage::helper('vendor')->__('Vendor information')));
-
-
-		$vendorField->addField('fname', 'text', array(
-            'label' => Mage::helper('vendor')->__('First Name'),
+    protected function _prepareForm()
+    {
+        $form = new Varien_Data_Form();
+        $this->setForm($form);
+        $fieldset = $form->addFieldset('idx_form',array('legend'=>Mage::helper('idx')->__('Upload file')));
+        $fieldset->addField('file', 'file', array(
+            'label' => Mage::helper('idx')->__('Upload Csv File'),
             'required' => true,
-            'name' => 'vendor[fname]',
-		));
-
-		$vendorField->addField('lname', 'text', array(
-            'label' => Mage::helper('vendor')->__('Last Name'),
-            'required' => true,
-            'name' => 'vendor[lname]',
-		));
-
-		$vendorField->addField('email', 'text', array(
-            'label' => Mage::helper('vendor')->__('Email'),
-            'required' => true,
-            'name' => 'vendor[email]',
-		));
-
-		$vendorField->addField('mobile', 'text', array(
-            'label' => Mage::helper('vendor')->__('Mobile'),
-            'required' => true,
-            'name' => 'vendor[mobile]',
-		));
-
-		$vendorField->addField('gender', 'select', array(
-            'label' => Mage::helper('vendor')->__('Gender'),
-            'required' => true,
-            'name' => 'vendor[gender]',
-            'type' => 'select',
-            'options' => array(
-            	'1' => Mage::helper('vendor')->__('Male'),
-            	'2' => Mage::helper('vendor')->__('Female'),
-
-            )
-		));
-
-		$vendorField->addField('status', 'select', array(
-            'label' => Mage::helper('vendor')->__('Status'),
-            'required' => true,
-            'name' => 'vendor[status]',
-            'type' => 'select',
-            'options' => array(
-            	'1' => Mage::helper('vendor')->__('Active'),
-            	'2' => Mage::helper('vendor')->__('Inactive'),
-            )
-		));
-
-		$vendorField->addField('company', 'text', array(
-            'label' => Mage::helper('vendor')->__('Company'),
-            'required' => true,
-            'name' => 'vendor[company]',
-		));
-
-		if ( Mage::getSingleton('adminhtml/session')->getvendorData() )
-		{
-			$form->setValues(Mage::getSingleton('adminhtml/session')->getvendorData());
-			Mage::getSingleton('adminhtml/session')->setvendorData(null);
-		} 
-		elseif ( Mage::registry('vendor_data') ) 
-		{
-			$form->setValues(Mage::registry('vendor_data')->getData());
-		}
-		return parent::_prepareForm();
-	}
+            'name' => 'import_options',
+        ));
+    }
 }
