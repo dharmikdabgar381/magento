43d3bb5c725d9a9af2dbbf1a1c50873f6554ec21
dharmikdabgar381 <dharmikdabgar381@gmail.com>
2023-05-31 14:06:50 +0530

    brand-module

---
+++ app/code/local/Dharmik/Brand/Helper/Data.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * 
+ */
+class Dharmik_Brand_Helper_Data extends Mage_Core_Helper_Abstract
+{
+	
+	function __construct()
+	{
+		// code...
+	}
+}
---
+++ app/code/local/Dharmik/Brand/Model/Mysql4/Brand.php
@@ -0,0 +1,6 @@
+<?php
+
+class Dharmik_Brand_Model_Mysql4_Brand extends Dharmik_Brand_Model_Resource_Brand
+{
+
+}
