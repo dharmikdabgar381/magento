ada6c85a8fd4916068b1958c1891af385ca53e93
dharmikdabgar381 <dharmikdabgar381@gmail.com>
2023-06-12 10:48:54 +0530

    featured-category 0.2.0 & 0.3.0

--- app/code/local/Mp/Brand/Model/Mysql4/Brand.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-class Mp_Brand_Model_Mysql4_Brand extends Mp_Brand_Model_Resource_Brand
-{
-
-}
--- app/code/local/Mp/Brand/Model/Mysql4/Brand/Collection.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-class Mp_Brand_Model_Mysql4_Brand_Collection extends Mp_Brand_Model_Resource_Brand_Collection
-{
-
-}
-
-?>
