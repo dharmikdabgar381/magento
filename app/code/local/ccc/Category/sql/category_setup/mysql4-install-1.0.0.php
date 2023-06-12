83afbdf5b1191ff8182451abe8327622cb258857
dharmikdabgar381 <dharmikdabgar381@gmail.com>
2023-06-12 09:59:27 +0530

    featured-category 0.1.0

--- app/code/local/ccc/Category/sql/category_setup/mysql4-install-1.0.0.php
+++ app/code/local/ccc/Category/sql/category_setup/mysql4-install-1.0.0.php
@@ -1,25 +1,18 @@
 <?php
 $installer = $this;
-
 $installer->startSetup();
-
 $installer->run("
-	DROP TABLE IF EXISTS `category`;
-CREATE TABLE `category` (
-  `category_id` int(11) NOT NULL,
-  `name` varchar(50) NOT NULL,
-  `status` tinyint(4) NOT NULL,
-  `description` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
-
-ALTER TABLE `category`
-  ADD PRIMARY KEY (`category_id`);
-
-ALTER TABLE `category`
-  MODIFY `category_id` int(11) NOT NULL AUTO_INCREMENT;
-
+  DROP TABLE IF EXISTS {$this->getTable('category')};
+  CREATE TABLE {$this->getTable('category')} (
+    `category_id` int(11) NOT NULL,
+    `name` varchar(255) NOT NULL,
+    `status` tinyint(4) NOT NULL,
+    `created_at` datetime NOT NULL,
+    `updated_at` datetime DEFAULT NULL
+  ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  ALTER TABLE {$this->getTable('category')}
+    ADD PRIMARY KEY (`category_id`);
+  ALTER TABLE {$this->getTable('category')}
+      MODIFY `category_id` int(11) NOT NULL AUTO_INCREMENT;
 ");
-
 $installer->endSetup();
-?>
